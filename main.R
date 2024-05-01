64f20eda16c1c0f42cce26923608179d42db68a2
Rolleen <loana20199@gmail.com>
2024-05-01 02:56:56 -0400

    Update maain.R
    Count amount of reserves with childs or babies

--- main.R
+++ main.R
@@ -2,7 +2,7 @@ file.choose()
 data=read.csv("C:\\Users\\USUARIO\\Documents\\CURSO - Fundamentos Data Science\\hotel_bookings.csv", header = TRUE, stringsAsFactors = FALSE)
 
 
-#1.¿Cuántas reservas se realizan por tipo de hotel? O ¿Qué tipo de hotel prefiere la gente?
+#¿Cuántas reservas se realizan por tipo de hotel? O ¿Qué tipo de hotel prefiere la gente?
 reservas_por_hotel <- table(data$hotel)
 print(reservas_por_hotel)
 
@@ -12,7 +12,7 @@ barplot(reservas_por_hotel,
         ylab = "Cantidad de Reservas",
         col = "skyblue")
 
-#2. ¿Está aumentando la demanda con el tiempo?
+# ¿Está aumentando la demanda con el tiempo?
 
 #creamos un vector que contenga todas reservas realizadas de acuerdo al año
 reservas_por_año <- aggregate(total_of_special_requests ~ arrival_date_year, data = data, FUN = sum)
@@ -30,5 +30,12 @@ ggplot(data = reservas_por_año, aes(x = arrival_date_year, y = total_of_special
        y = "Cantidad total de reservas")
 
 
+# ¿Cuántas reservas incluyen niños y/o bebés?
 
+# Contar el número de reservas que incluyen niños y/o bebés
+
+reservas_con_niños <- sum(!is.na(hotel_bookings_TP$children) & hotel_bookings_TP$children > 0 |
+                            !is.na(hotel_bookings_TP$babies) & hotel_bookings_TP$babies > 0)
+
+print(paste("Cantidad total de reservas que incluyen niños y/o bebés:", reservas_con_niños))
 
